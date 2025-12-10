b9b05d83807039ce0833ecde1b7daf695a941d1c
Nicolas <nicolasespinoza24raac@gmail.com>
2025-12-10 04:53:52 -0300

    actualización

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Perfil(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil', default=1)
     saldo = models.DecimalField(max_digits=10, decimal_places=0, default=0)
 
     def __str__(self):
